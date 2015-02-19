61d06e5f38fad03ca0763e0564175abd72d436ec
Sam G. Feigenbaum <sfeigen@live.com>
2015-02-19 15:34:54 -0500

    troubleshooting carosel issues

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,2 +1,7 @@
 //Footer Date
 $('.footer').append(new Date().getFullYear(), " &copy;  Nakba Museum Project. All Rights Reserved.");
+
+//Navbar Auto-close
+$('.navbar-collapse').click('li', function() {
+	$('.navbar-collapse').collapse('hide');
+});
