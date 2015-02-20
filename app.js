4efa9ccc01981cb8e50622dac557875fa99e3461
Sam G. Feigenbaum <sfeigen@live.com>
2015-02-19 21:56:19 -0500

    template added

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 (function() {
-	var app = angular.module('app', []);
+	var app = angular.module('app', [ 'ui.router' ]);
+
+	app.config(function($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.otherwise("#");
+
+		$stateProvider
+		.state('about', {
+			url: "/about",
+			templateUrl: "Partials/about.html"
+		})
+		.state('contact', {
+			url: "/contact",
+			templateUrl: "Partials/contact.html"
+		})
+		.state('gallery', {
+			url: "/gallery",
+			templateUrl: "Partials/gallery.html"
+		})
+		.state('support', {
+			url: "/support",
+			templateUrl: "Partials/support.html"
+		})
+		.state('mission', {
+			url: "/mission",
+			templateUrl: "Partials/mission.html" 
+		})
+	});
 })();
