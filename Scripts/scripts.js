ae3cec10a0b06792f5d03734405dd0ec61666006
Jupiter <sfeigen@live.com>
2015-02-19 19:28:47 -0500

    carousel and design

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -5,3 +5,23 @@ $('.footer').append(new Date().getFullYear(), " &copy;  Nakba Museum Project. Al
 $('.navbar-collapse').click('li', function() {
 	$('.navbar-collapse').collapse('hide');
 });
+
+//Carousel Controls
+$(document).ready(function() {
+	$("#carouselMain").owlCarousel({
+ 
+      navigation : false, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+      autoPlay: 5000
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+});
